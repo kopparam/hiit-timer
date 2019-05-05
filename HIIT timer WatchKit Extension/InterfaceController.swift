9fb914870a4aebf3ca2168f75d710c1080e401fc
Kashyap Kopparam <kashyapkopparam@gmail.com>
2019-05-05 15:37:15 +0800

    implements basic interval timer
    - static interval times
    - preperation timer with 3 second prompt
    
    Signed-off-by: Kashyap Kopparam <kashyapkopparam@gmail.com>

--- HIIT timer WatchKit Extension/InterfaceController.swift
+++ HIIT timer WatchKit Extension/InterfaceController.swift
@@ -21,11 +21,74 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        activityLabel.setText("")
+        totalInterval = workInterval + restInterval
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
+    let workInterval = 4.0
+    let restInterval = 1.0
+    var totalInterval = 0.0
+    var workCount = 0
+    var restCount = 0
+    var reset = false
+    
+    @IBOutlet weak var activityLabel: WKInterfaceLabel!
+    
+    fileprivate func startWorkout(_ totalInterval: Double) {
+        activityLabel.setText("Work \(workCount)")
+        Timer.scheduledTimer( withTimeInterval: totalInterval, repeats: true) { timer in
+            if self.reset {
+                timer.invalidate()
+            } else {
+                self.workCount = self.workCount + 1
+                self.activityLabel.setText("Work \(self.workCount)")
+            }
+        }
+        Timer.scheduledTimer(withTimeInterval: workInterval, repeats: false) { timer in
+            self.restCount = self.restCount + 1
+            self.activityLabel.setText("Rest \(self.restCount)")
+            Timer.scheduledTimer(withTimeInterval: totalInterval, repeats: true) { restTimer in
+                if self.reset {
+                    restTimer.invalidate()
+                } else {
+                    self.restCount = self.restCount + 1
+                    self.activityLabel.setText("Rest \(self.restCount)")
+                }
+            }
+            timer.invalidate()
+        }
+    }
+    
+    fileprivate func prepareForWorkout() {
+        activityLabel.setText("Get ready!")
+        
+        var readyCountdown = 3
+        Timer.scheduledTimer( withTimeInterval: 1.0, repeats: true) { timer in
+            if readyCountdown == 0 {
+                timer.invalidate()
+                self.startWorkout(self.totalInterval)
+            } else {
+                self.activityLabel.setText("\(readyCountdown)")
+                readyCountdown = readyCountdown - 1
+            }
+        }
+    }
+    
+    @IBAction func startButton() {
+        reset = false
+        workCount = workCount + 1
+        
+        prepareForWorkout()
+    }
+    @IBAction func stopButton() {
+        activityLabel.setText("Great workout!")
+        reset = true
+        workCount = 0
+        restCount = 0
+    }
 }
--- HIIT timer/ViewController.swift
+++ HIIT timer/ViewController.swift
@@ -8,13 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return intervalData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return intervalData[row]
+    }
+    
+    func view(viewForRow row: Int, forComponent component: Int) -> UIView {
+        
+        
+        // Display the first column's width
+        // The value 0 is to point to the first column regardless if you have one or multiple columns
+        
+        let pickerLabel : UILabel = UILabel()
+        
+        // pickerViewLabelTextForComponentRow is a custom made function to obtain what text will go in each row
+        pickerLabel.text = "sss"
+        pickerLabel.textAlignment = NSTextAlignment.center
+        pickerLabel.textColor = UIColor.black
+        pickerLabel.font = UIFont.boldSystemFont(ofSize: 13.0)
+        pickerLabel.backgroundColor = UIColor.lightGray
+        
+        // If you wish to set up the labels individually in each column, use the value of the parameter component with a control condition such as if or switch
+        
+        return pickerLabel
+    }
 
+    @IBOutlet weak var interval: UIPickerView!
+
+    var intervalData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.interval.delegate = self
+        self.interval.dataSource = self
+        
+        intervalData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
     }
 
-
+    
 }
-
