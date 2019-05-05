9833857b97d1951f8917b110eb8413f0f76aa3bb
Kashyap Kopparam <kashyapkopparam@gmail.com>
2019-05-05 15:37:11 +0800

    Initial Commit
    Signed-off-by: Kashyap Kopparam <kashyapkopparam@gmail.com>

---
+++ HIIT timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HIIT timer
+//
+//  Created by Kashyap Kopparam on 14/4/19.
+//  Copyright © 2019 Kashyap Kopparam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HIIT timerTests/HIIT_timerTests.swift
@@ -0,0 +1,34 @@
+//
+//  HIIT_timerTests.swift
+//  HIIT timerTests
+//
+//  Created by Kashyap Kopparam on 14/4/19.
+//  Copyright © 2019 Kashyap Kopparam. All rights reserved.
+//
+
+import XCTest
+@testable import HIIT_timer
+
+class HIIT_timerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
